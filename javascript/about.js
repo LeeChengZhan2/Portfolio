0a0aa3811364aa506b378c4b347234623e1f7947
LeeChengZhan <leechengzhan7@gmail.com>
2023-04-18 00:28:55 +0800

    Reupload for rename folder

---
+++ javascript/about.js
@@ -0,0 +1,30 @@
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+// $(document).ready(function(){
+//     var tablinks = $(".tab-links");
+//     var tabcontents = $(".tab-contents");
+
+//     function opentab(tabname){
+//         for(tablink of tablinks){
+//             tablink.classList.remove("active-link");
+//         }
+//         for(tabcontent of tabcontents){
+//             tabcontent.classList.remove("active-tab");
+//         }
+//         $(event.currentTarget).addClass("active-link");
+//         $("#" + tabname).addClass("active-tab");
+//     }
+// });
